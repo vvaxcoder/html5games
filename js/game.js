2c3e0f9dff2b5484d0d5609bcadc13b9f499fdb1
Valerii Voronkov <scorpirat@gmail.com>
2021-05-23 20:53:39 +0300

    Add blocks.

--- js/game.js
+++ js/game.js
@@ -1,11 +1,15 @@
 let game = {
     ball: null,
+    blocks: [],
+    rows: 4,
+    columns: 8,
     ctx: null,
     platform: null,
     sprites: {
         background: null,
         ball: null,
         platform: null,
+        block: null,
     },
     init: function () {
         this.ctx = document.querySelector("#myCanvas").getContext("2d");
@@ -13,16 +17,17 @@ let game = {
     preload(callback) {
         let loaded = 0;
         let required = Object.keys(this.sprites).length;
+        const onImageLoad = () => {
+            loaded++;
+            if (loaded >= required) {
+                callback();
+            }
+        };
 
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = `img/${key}.png`;
-            this.sprites[key].addEventListener("load", () => {
-                loaded++;
-                if (loaded >= required) {
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener("load", onImageLoad);
         }
     },
     render() {
@@ -30,6 +35,20 @@ let game = {
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x,
                            this.ball.y, this.ball.width, this.ball.height);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+        this.renderBlocks();
+    },
+    renderBlocks() {
+        for (const block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y);
+        }
+    },
+    create() {
+        for (let row = 0; row < this.rows; row++) {
+            for (let column = 0; column < this.columns; column++) {
+                this.blocks = [...this.blocks, { x: (64 * column) + 65, y: (24 * row) + 35 }];
+                // this.blocks.push({ x: 60 * column, y: 20 * row });
+            }
+        }
     },
     run() {
         window.requestAnimationFrame(() => {
@@ -39,6 +58,7 @@ let game = {
     start: function () {
         this.init();
         this.preload(() => {
+            this.create();
             this.run();
         });
         this.render();
