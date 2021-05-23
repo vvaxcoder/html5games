0117d8eb2b4046958d4c4f95aab56ef8709defac
Valerii Voronkov <scorpirat@gmail.com>
2021-05-23 19:46:35 +0300

    Draw a part of the ball.

--- js/game.js
+++ js/game.js
@@ -1,11 +1,13 @@
 let game = {
+    ball: null,
     ctx: null,
+    platform: null,
     sprites: {
         background: null,
         ball: null,
         platform: null,
     },
-    init: function() {
+    init: function () {
         this.ctx = document.querySelector("#myCanvas").getContext("2d");
     },
     preload(callback) {
@@ -25,8 +27,9 @@ let game = {
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
-        this.ctx.drawImage(this.sprites.ball, 0, 0);
-        this.ctx.drawImage(this.sprites.platform, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x,
+                           this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
     },
     run() {
         window.requestAnimationFrame(() => {
@@ -40,6 +43,18 @@ let game = {
         });
         this.render();
     }
+};
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
 };
 
 window.addEventListener('load', () => {
