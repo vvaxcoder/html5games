cd799368ac24e3e847dae4fdddd1db0227888160
Valerii Voronkov <scorpirat@gmail.com>
2021-06-26 23:02:01 +0300

    Change the angle of the ball.

--- js/game.js
+++ js/game.js
@@ -9,6 +9,8 @@ let game = {
     blocks: [],
     rows: 4,
     columns: 8,
+    width: 640,
+    height: 360,
     ctx: null,
     platform: null,
     sprites: {
@@ -53,6 +55,7 @@ let game = {
         }
     },
     render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height, this.ball.x,
                            this.ball.y, this.ball.width, this.ball.height);
@@ -90,7 +93,10 @@ let game = {
             this.run();
         });
         this.render();
-    }
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    },
 };
 
 game.ball = {
@@ -104,8 +110,12 @@ game.ball = {
         if (this.dy) {
             this.y += this.dy;
         }
+        if (this.dx) {
+            this.x += this.dx;
+        }
     },
     start() {
+        this.dx = game.random(-this.velocity, this.velocity);
         this.dy = -this.velocity;
     }
 };
