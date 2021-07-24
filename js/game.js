6735b0761c7a0275d2d4cc349a95b1de62d48dbe
Valerii Voronkov <scorpirat@gmail.com>
2021-07-24 14:43:52 +0300

    Dynamic change the size of game board.

--- js/game.js
+++ js/game.js
@@ -2,16 +2,50 @@ let game = {
     board: null,
     canvas: null,
     ctx: null,
-    height: 360,
+    dimensions: {
+        max: {
+            height: 360,
+            width: 640
+        },
+        min: {
+            height: 300,
+            width: 300
+        },
+    },
+    fitHeight(data) {
+        this.width = Math.floor((data.realwidth * data.maxHeight) / data.realHeight);
+        this.width = Math.min(this.width, data.minWidth);
+        this.width = Math.max(this.width, data.maxWidth);
+        this.height = Math.floor((this.width * data.realHeight) / data.realwidth);
+        this.canvas.style.height = '100%';
+    },
+    height: 0,
+    initDimensions() {
+        let data = {
+            maxWidth: this.dimensions.max.width,
+            maxHeight: this.dimensions.max.height,
+            minWidth: this.dimensions.min.width,
+            minHeight: this.dimensions.min.height,
+            realwidth: window.innerWidth,
+            realHeight: window.innerHeight,
+        };
+
+        this.fitHeight(data);
+
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        
+    },
     sprites: {
         background: null,
         body: null,
         cell: null,
     },
-    width: 640,
+    width: 0,
     init() {
         this.canvas = document.getElementById("mycanvas");
         this.ctx = this.canvas.getContext("2d");
+        this.initDimensions();
     },
     start() {
         this.init();
@@ -45,7 +79,7 @@ let game = {
         this.snake.create();
 
         window.requestAnimationFrame(() => {
-            this.ctx.drawImage(this.sprites.background, 0, 0);
+            this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width)/2, (this.height - this.sprites.background.height)/2);
             this.board.render();
             this.snake.render();
         });
