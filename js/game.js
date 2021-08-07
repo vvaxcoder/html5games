df4c2ad4ff7727a1dbbc07d2f3abf50d5e65d287
Valerii Voronkov <scorpirat@gmail.com>
2021-08-07 21:52:01 +0300

    Show text on canvas.

--- js/game.js
+++ js/game.js
@@ -1,80 +1,35 @@
 let game = {
-    board: null,
-    bombInterval: 0,
     canvas: null,
     ctx: null,
+    board: null,
+    width: 0,
+    height: 0,
+    score: 0,
     dimensions: {
         max: {
-            height: 360,
-            width: 640
+            width: 640,
+            height: 360
         },
         min: {
-            height: 300,
-            width: 300
-        },
-    },
-    fitHeight(data) {
-        this.width = Math.floor((data.realWidth * data.maxHeight) / data.realHeight);
-        this.width = Math.min(this.width, data.maxWidth);
-        this.width = Math.max(this.width, data.minWidth);
-        this.height = Math.floor((this.width * data.realHeight) / data.realWidth);
-        this.canvas.style.height = '100%';
-    },
-    fitWidth(data) {
-        this.height = Math.floor((this.width * data.realHeight)/data.realWidth);
-        this.height = Math.min(this.height, data.maxHeight);
-        this.height = Math.max(this.height, data.minHeight);
-        this.width = Math.floor((data.realWidth * this.height) / data.realHeight);
-        this.canvas.style.width = '100%';
-    },
-    gameInterval: 0,
-    height: 0,
-    initDimensions() {
-        let data = {
-            maxWidth: this.dimensions.max.width,
-            maxHeight: this.dimensions.max.height,
-            minWidth: this.dimensions.min.width,
-            minHeight: this.dimensions.min.height,
-            realWidth: window.innerWidth,
-            realHeight: window.innerHeight,
-        };
-
-        if ((data.realWidth/data.realHeight) > (data.maxWidth/data.maxHeight)) {
-            this.fitWidth(data);
-        } else {
-            this.fitHeight(data);
+            width: 300,
+            height: 300
         }
-
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-        
     },
-    onSnakeStart() {
-        this.sounds.theme.loop = true;
-        this.sounds.theme.play();
-    },
-    onSnakeEat() {
-        this.sounds.food.play();
-        this.board.createFood();
+    sprites: {
+        background: null,
+        cell: null,
+        head: null,
+        body: null,
+        food: null,
+        bomb: null,
     },
     sounds: {
         bomb: null,
         food: null,
-        theme: null,
+        theme: null
     },
-    sprites: {
-        background: null,
-        body: null,
-        bomb: null,
-        cell: null,
-        food: null,
-        head: null,
-    },
-    width: 0,
-    init() {
-        this.canvas = document.getElementById("mycanvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.initDimensions();
+    random(min, max) {
+        return Math.floor(Math.random() * (max + 1 - min)) + min;
     },
     start() {
         this.init();
@@ -82,12 +37,48 @@ let game = {
             this.run();
         });
     },
-    stop() {
-        this.sounds.bomb.play();
-        clearInterval(this.gameInterval);
-        clearInterval(this.bombInterval);
-        alert('Game over');
-        window.location.reload();
+    init() {
+        this.canvas = document.getElementById("mycanvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.initDimensions();
+        this.setTextFont();
+    },
+    setTextFont() {
+        this.ctx.font = "20px Arial";
+        this.ctx.fillStyle = "#FFFFFF";
+    },
+    initDimensions() {
+        let data = {
+            maxWidth: this.dimensions.max.width,
+            maxHeight: this.dimensions.max.height,
+            minWidth: this.dimensions.min.width,
+            minHeight: this.dimensions.min.height,
+            realWidth: window.innerWidth,
+            realHeight: window.innerHeight
+        };
+
+        if (data.realWidth/data.realHeight > data.maxWidth/data.maxHeight) {
+            this.fitWidth(data);
+        } else {
+            this.fitHeight(data);
+        }
+
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+    },
+    fitWidth(data) {
+        this.height = Math.round(this.width * data.realHeight / data.realWidth);
+        this.height = Math.min(this.height, data.maxHeight);
+        this.height = Math.max(this.height, data.minHeight);
+        this.width = Math.round(data.realWidth * this.height / data.realHeight);
+        this.canvas.style.width = "100%";
+    },
+    fitHeight(data) {
+        this.width = Math.round(data.realWidth * data.maxHeight / data.realHeight);
+        this.width = Math.min(this.width, data.maxWidth);
+        this.width = Math.max(this.width, data.minWidth);
+        this.height = Math.round(this.width * data.realHeight / data.realWidth);
+        this.canvas.style.height = "100%";
     },
     preload(callback) {
         let loaded = 0;
@@ -97,68 +88,84 @@ let game = {
             ++loaded;
 
             if (loaded >= required) {
-                // when all sprites will be upload
                 callback();
             }
         };
-
         this.preloadSprites(onAssetLoad);
         this.preloadSounds(onAssetLoad);
     },
-    preloadSprites(onAssetLoad) {
+    preloadSprites(onAssetLoad){
         for (let key in this.sprites) {
             this.sprites[key] = new Image();
-            this.sprites[key].src = "img/" + key + ".png"; 
+            this.sprites[key].src = "img/" + key + ".png";
             this.sprites[key].addEventListener("load", onAssetLoad);
         }
     },
     preloadSounds(onAssetLoad) {
         for (let key in this.sounds) {
             this.sounds[key] = new Audio();
-            this.sounds[key].src = "sounds/" + key + ".mp3"; 
-            this.sounds[key].addEventListener("canplaythrough", onAssetLoad, { once: true });
+            this.sounds[key].src = "sounds/" + key + ".mp3";
+            this.sounds[key].addEventListener("canplaythrough", onAssetLoad, {once: true});
         }
     },
     create() {
-        // create game objects
+        // создание игровых объектов
         this.board.create();
         this.snake.create();
         this.board.createFood();
         this.board.createBomb();
-        // set game events
-        window.addEventListener('keydown', (e) => {
+        // установка игровых событий
+        window.addEventListener("keydown", e => {
             this.snake.start(e.keyCode);
         });
     },
-    random(min, max) {
-        return Math.floor(Math.random() * (max + 1 - min) + min);
-    },
     render() {
-        // render game objects
+        // отрисовка игровых объектов
         window.requestAnimationFrame(() => {
+            // перед тем, как отрисовать новый кадр необходимо очистить предыдущий
             this.ctx.clearRect(0, 0, this.width, this.height);
-            this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width)/2, (this.height - this.sprites.background.height)/2);
+            this.ctx.drawImage(this.sprites.background, (this.width - this.sprites.background.width) / 2, (this.height - this.sprites.background.height) / 2);
             this.board.render();
             this.snake.render();
+            this.ctx.fillText("Score: " + this.score, 30, 30);
         });
     },
     update() {
+        // двигать змею
         this.snake.move();
+        // отрисовывать новый кадр
         this.render();
     },
     run() {
         this.create();
-        // move snake every 100ms and render new frame
+        // каждые 150мс
         this.gameInterval = setInterval(() => {
             this.update();
         }, 150);
 
+        // каждые 3с
         this.bombInterval = setInterval(() => {
             if (this.snake.moving) {
                 this.board.createBomb();
             }
         }, 3000);
     },
+    stop() {
+        this.sounds.bomb.play();
+        clearInterval(this.gameInterval);
+        clearInterval(this.bombInterval);
+        alert("Игра завершена");
+        window.location.reload();
+    },
+    onSnakeStart() {
+        this.sounds.theme.loop = true;
+        this.sounds.theme.play();
+    },
+    onSnakeEat() {
+        ++this.score;
+        this.sounds.food.play();
+        this.board.createFood();
+    }
 };
 
 game.start();
